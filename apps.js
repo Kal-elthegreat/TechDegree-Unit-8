ea1fd97343c11c900d66331fa87dcec2f4f48aea
Kiel Lewis <kiellewis@Kiels-MacBook-Pro.local>
2019-02-18 11:35:52 -0500

    Initial save
    Environmet set up. Time to build.

---
+++ apps.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+
+app.set('view engine', 'pug');
+
+//routes
+app.get('/',(req,res) => { // home page
+    res.redirect('/books')// redirect to /books
+})
+
+app.get('/books',(req,res) => {
+    res.render('index')
+})
+
+app.get('/books/new',(req,res) => {
+    res.render('new-book')
+})
+
+// post books/new
+
+app.get('/books/:id', (req,res) => {
+    res.render('update-book')
+})
+
+//post books/:id
+
+//post books/:id/delete
+
+/*app.listen(3000, () => {
+    console.log('App running on port 3000')
+});*/
+
+module.exports = app;
