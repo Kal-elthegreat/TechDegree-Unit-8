4893a25604fdb3c43937a5972d4cf199387ce1f5
Kiel Lewis <kiellewis@Kiels-MacBook-Pro.local>
2019-02-21 13:24:00 -0500

    Major improvement.
    New book routes working.
    
    Needs fixing:
    Update route keeps creating a new row instead of updating the selected.
    
    Next step:
    find a way to delete rows

--- routes/books.js
+++ routes/books.js
@@ -4,32 +4,41 @@ var Book = require("../models").Book;
 
 
 router.get('/books',(req,res) => {
-    Book.findAll().then(books => {
-        res.render('index', { books: books, },
-        //  console.log(books)
-         )
+    Book.findAll().then(books => { // select all books 
+        res.render('index', { books: books, }) // display index.pug w/ books data
       })
     })
   
+    // get books/new
   router.get('/books/new',(req,res) => {
-    res.render('new-book');
+    res.render('new-book', ); // render new-book form
   })
   
   // post books/new 
-  router.post('/books/new',(req,res) => {
-    //req
+  router.post('/books',(req,res) => {
+    Book.create(req.body).then( () => { // create new book and post to /books route
+      res.redirect('/books/')
+    })
   })
   
+  // get books/id (update-book)
   router.get('/books/:id', (req,res) => {
-    res.render('update-book')
+      Book.findById(req.params.id).then(books => {  // use params.id to select the correct book from data
+        res.render('update-book', { books: books} )
+      })
   })
   
-  //post books/:id
-  router.post('/books/:id',(req,res) => {
+  //post books/:id (updated book)
+  router.put('/books/:id',(req,res) => {
+    Book.findById(req.params.id).then( books => {
+      return books.update(req.body)
+    }).then( books => {
+      res.redirect('/books/')
+    })
     //req
   })
   
-  //post books/:id/delete
+  //post books/:id/delete (index.pug w/o deleted book)
   router.post('/books/:id/delete',(req,res) => {
     //req
   })
