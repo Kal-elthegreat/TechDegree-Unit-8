b7aad3979bd16956ab2f03dbcf5299ae9491c2ed
Kiel Lewis <kiellewis@Kiels-MacBook-Pro.local>
2019-02-22 11:23:38 -0500

    adding error routes

--- routes/books.js
+++ routes/books.js
@@ -6,47 +6,67 @@ var Book = require("../models").Book;
 router.get('/books',(req,res) => {
     Book.findAll().then(books => { // select all books 
         res.render('index', { books: books, }) // display index.pug w/ books data
+      }).catch(err => {
+        res.send(500);
       })
-    })
+    });
   
     // get books/new
   router.get('/books/new',(req,res) => {
     res.render('new-book', ); // render new-book form
-  })
+  });
   
   // post books/new  
   router.post('/books/new',(req,res) => {
     Book.create(req.body).then( () => { // create new book and post to /books route
       res.redirect('/books')
+    }).catch( err => {
+      res.send(500);
     })
-  })
+  });
   
   // get books/id (update-book)
   router.get('/books/:id', (req,res) => {
       Book.findById(req.params.id).then(books => {  // use params.id to select the correct book from data
-        res.render('update-book', { books: books} )
+        if(books){
+          res.render('update-book', { books: books});
+        } else {
+          res.render('error');
+        }
+      }).catch(err => {
+        res.send(500);
       })
-  })
+  });
   
   //post books/:id (updated book)
   router.post('/books/:id',(req,res) => {
-    Book.findById(req.params.id).then(books => {
-      return books.update(req.body)
+    Book.findById(req.params.id).then(books => { // find book
+      if(books){
+        return books.update(req.body); // update values
+      } else {
+        res.send(404);
+      }
     }).then( () => {
       res.redirect('/books')
+    }).catch(err => {
+      res.send(500);
     })
-    //req
-  })
+  });
   
   //post books/:id/delete (index.pug w/o deleted book)
   router.post('/books/:id/delete',(req,res) => {
     Book.findById(req.params.id).then( books =>{
-      return books.destroy();
+      if(books){
+        return books.destroy();
+      } else {
+        res.send(404);
+      } 
     }).then( () =>{
       res.redirect('/books')
+    }).catch(err => {
+      res.send(500);
     })
-    //req
-  })
+  });
   
 
   module.exports = router;
